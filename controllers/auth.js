9dc17b39b77eddd892ace6f29b3afeb1e91dca61
Sherwynd Liew <sherwynd123@gmail.com>
2023-12-17 17:58:31 +0800

    modify login and added token authorisation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,9 @@
-
 const User = require('../models/user')
+const Token = require('../models/token')
 const bcrypt = require('bcrypt')
 const { v4: uuidv4 } = require('uuid')
 const { validateUser } = require('../validations/auth')
+const jwt = require('jsonwebtoken')
 
 const registerAccount = async (req,res) => {
     try {
@@ -34,13 +35,24 @@ const getAllAccount = async (req,res) => {
 }
 
 const loginAccount = async (req,res) => {
-  const loginUser = await User.findOne({ userName: req.body.username })
-  if(loginUser == null){
-    return res.status(400).send('User not found')
-  }
   try{
+    const loginUser = await User.findOne({
+      $or: [
+        { username: req.body.username },
+        { email: req.body.email }
+      ]
+    })
+    if(!loginUser) {
+      return res.status(400).send('User or Email not found')
+    }
+    const loginUsername = { username: loginUser.username }
     if(await bcrypt.compare (req.body.password, loginUser.password)) {
-      res.send('Login Success')
+      const accessToken = generateAccessToken(loginUsername)
+      const refreshToken = jwt.sign(loginUsername, process.env.REFRESH_TOKEN_SECRET)
+      const newToken = new Token({ token: refreshToken })
+      await newToken.save()
+      res.json({ accessToken: accessToken, refreshToken: refreshToken })
+      // res.send('Login Success')
     } else {
       res.status(500).send('Login Failed')
     }
@@ -64,7 +76,7 @@ const editAccount = async (req,res) => {
 
 const deleteAccount = async (req,res) => {
   try {
-      const deleteUser = await User.findByIdAndDelete(req.params.id);
+      const deleteUser = await User.findByIdAndDelete(req.params.id)
       if(deleteUser == null){
         res.status(400).send('Cant find User')
       } else {
@@ -72,16 +84,75 @@ const deleteAccount = async (req,res) => {
       }
     }
     catch (err) {
-      handleErrors(res, err);
+      handleErrors(res, err)
     }
 }
 
+const authenticateToken = async (req, res, next) => {
+  try{
+    const findUserAll = await User.find()
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if(token == null) return res.status(401).send('Token not provided');
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+      if(err) return res.status(403).send('Token is not valid');
+      req.user = user
+      res.json(findUserAll.filter(user => user.username == req.user.username))
+    })
+  }catch (err) {
+    handleErrors(res, err)
+  }
+}
+
+const generateAccessToken = (user) => {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '30s'})
+}
+
+const refreshToken = async (req, res) => {
+  try{
+    const refreshToken = req.body.token
+
+    if(refreshToken == null) return res.status(401).send('Refresh Token not provided')
+    
+    const getToken = await Token.findOne({ token: req.body.token })
+    
+    if(getToken == null) return res.status(401).send('Refresh Token not found in data')
+    
+    const getRefreshToken = { token: getToken.token }
+    
+    if(getRefreshToken == null) return res.status(403).send('Refresh Token is invalid')
+   
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+      if(err) return res.status(403).send(err)
+      const accessToken = generateAccessToken({ username: user.username })
+      res.json({ accessToken:accessToken })
+    })
+  }catch (err) {
+    handleErrors(res, err)
+  }
+}
+
+const logoutAccount = async (req, res) => {
+  try {
+    const result = await Token.deleteOne({ token: req.body.token });
+
+    if (result.deletedCount === 0) {
+      return res.status(400).send("Can't find Token");
+    } else {
+      res.status(204).send("Token deleted successfully");
+    }
+  }
+  catch (err) {
+    handleErrors(res, err)
+  }
+}
+
+
 // Reusable middleware function for handling errors
-const handleErrors = async (res, err) => {
+const handleErrors = (res, err) => {
   console.log(err)
   res.status(500).json(err)
 }
-
 
 module.exports = {
   registerAccount,
@@ -89,4 +160,8 @@ module.exports = {
   getAllAccount,
   editAccount,
   deleteAccount,
+  authenticateToken,
+  generateAccessToken,
+  logoutAccount,
+  refreshToken,
 }
--- routers/auth.js
+++ routers/auth.js
@@ -6,10 +6,12 @@ const authController = require('../controllers/auth')
 //auth/
 
 router.get('/', authController.getAllAccount)//Get ALL
-// router.get('/:id', authController.getIdAccount) //Get ids
+router.get('/getToken', authController.authenticateToken) //Get ids
+router.post('/refreshToken', authController.refreshToken) //Create
 router.post('/registerAccount', authController.registerAccount) //Create
 router.post('/loginAccount', authController.loginAccount) //Create
 router.delete('/deleteAccount/:id', authController.deleteAccount) //Delete
+router.delete('/logoutAccount', authController.logoutAccount) //Delete
 router.patch('/editAccount/:id', authController.editAccount) //Patch
 
 
