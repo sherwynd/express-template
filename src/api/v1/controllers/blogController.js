02ec68d78b6a3c659b2f0b40f763a114c34d509f
Sherwynd Liew <sherwynd123@gmail.com>
2024-06-04 02:07:04 +0800

    Comment username added

--- src/api/v1/controllers/blogController.js
+++ src/api/v1/controllers/blogController.js
@@ -6,143 +6,151 @@ const { v4: uuidv4 } = require("uuid");
 
 // GET all blogs
 const getAllBlogs = async (req, res) => {
-    try {
-        const blogs = await Blog.find({}).sort({createdAt: -1});
-        res.status(200).json(blogs);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+  try {
+    const blogs = await Blog.find({}).sort({ createdAt: -1 });
+    res.status(200).json(blogs);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 // GET a single blog by ID
 const getBlogById = async (req, res) => {
-    try {
-        const { id } = req.params;
-        console.log("this is my ID: ", id);
-        // console.log(`Received ID: ${id}`);
-    
-        if (!mongoose.Types.ObjectId.isValid(id)) {
-          console.log("Invalid ObjectId");
-    
-          res.status(404).json({ error: "Test Blog not found" });
-        }
-    
-        const blog = await blogService.getBlogById(id);
-        if (!blog) {
-          return res.status(404).json({ message: "Blog not found" });
-        }
-        res.status(200).json(blog);
-      } catch (error) {
-        res.status(500).json({ message: error.message });
-      }
-    // try
-    //     {
-    //     const { id } = req.params;
-    //     const blog = await blogService.getBlogById(id);
-    //     if (!blog) {
-    //     return res.status(404).json({ message: "Blog not found" });
-    //     }
-    //     res.status(200).json(blog);
-    // } catch (error) {
-    //     res.status(500).json({ message: error.message });
-    // }
+  try {
+    const { id } = req.params;
+    console.log("this is my ID: ", id);
+    // console.log(`Received ID: ${id}`);
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      console.log("Invalid ObjectId");
+
+      res.status(404).json({ error: "Test Blog not found" });
+    }
+
+    const blog = await blogService.getBlogById(id);
+    if (!blog) {
+      return res.status(404).json({ message: "Blog not found" });
+    }
+    res.status(200).json(blog);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+  // try
+  //     {
+  //     const { id } = req.params;
+  //     const blog = await blogService.getBlogById(id);
+  //     if (!blog) {
+  //     return res.status(404).json({ message: "Blog not found" });
+  //     }
+  //     res.status(200).json(blog);
+  // } catch (error) {
+  //     res.status(500).json({ message: error.message });
+  // }
 };
 
 // CREATE a new blog
 const createBlog = async (req, res) => {
-    const { heading, description, comments, favouriteCount, creatorId } = req.body;
-    const images = req.files.map(file => `uploads/${file.filename}`);
+  const { heading, description, comments, favouriteCount, creatorId } =
+    req.body;
+  const images = req.files.map((file) => `uploads/${file.filename}`);
 
-    try {
-        const blog = await Blog.create({
-            heading,
-            description,
-            images,
-            comments,
-            favouriteCount,
-            creatorId
-        })
-        res.status(200).json(blog);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+  try {
+    const blog = await Blog.create({
+      heading,
+      description,
+      images,
+      comments,
+      favouriteCount,
+      creatorId,
+    });
+    res.status(200).json(blog);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 const createComment = async (req, res) => {
-    const { id } = req.params;
-    const { text } = req.body;
-  
-    try {
-      const blog = await Blog.findById(id);
-      if (!blog) {
-        return res.status(404).json({ error: 'Blog not found' });
-      }
-  
-      const comment = { text, blogId: id, refId: uuidv4() };
-      blog.comments.push(comment);
-      await blog.save();
-  
-      res.status(201).json(blog);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
+  const { id } = req.params;
+  const { text, userId } = req.body;
+
+  try {
+    const blog = await Blog.findById(id);
+    if (!blog) {
+      return res.status(404).json({ error: "Blog not found" });
     }
-  };
+
+    const user = await User.findById(userId);
+    if (!blog) {
+      return res.status(404).json({ error: "Blog not found" });
+    }
+    const username = user.username;
+
+    const comment = { text, username, refId: uuidv4() };
+    blog.comments.push(comment);
+    await blog.save();
+
+    res.status(201).json(blog);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 // UPDATE an existing blog
 const updateBlog = async (req, res) => {
-    try {
-        const { refId, blogId } = req.params;
+  try {
+    const { refId, blogId } = req.params;
 
-        // Fetch the blog to check ownership
-        const blog = await Blog.findById(blogId);
-        if (!blog) {
-            return res.status(404).json({ message: "Blog not found" });
-        }
-
-        // Check if the logged-in user is the owner of the blog
-        if (blog.creatorId !== refId) {
-            return res.status(403).json({ message: "You do not have permission to edit this blog" });
-        }
-
-        // Allow only heading and description to be updated
-        const updateData = {
-            heading: req.body.heading,
-            description: req.body.description,
-        };
-
-        // Proceed with the update if the user is the owner
-        const updatedBlog = await blogService.updateBlog(blogId,refId, updateData);
-        if (!updatedBlog) {
-            return res.status(404).json({ message: "Blog not found" });
-        }
-        
-        res.status(200).json(updatedBlog);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+    // Fetch the blog to check ownership
+    const blog = await Blog.findById(blogId);
+    if (!blog) {
+      return res.status(404).json({ message: "Blog not found" });
     }
-};
 
+    // Check if the logged-in user is the owner of the blog
+    if (blog.creatorId !== refId) {
+      return res
+        .status(403)
+        .json({ message: "You do not have permission to edit this blog" });
+    }
+
+    // Allow only heading and description to be updated
+    const updateData = {
+      heading: req.body.heading,
+      description: req.body.description,
+    };
+
+    // Proceed with the update if the user is the owner
+    const updatedBlog = await blogService.updateBlog(blogId, refId, updateData);
+    if (!updatedBlog) {
+      return res.status(404).json({ message: "Blog not found" });
+    }
+
+    res.status(200).json(updatedBlog);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 // DELETE a blog
 const deleteBlog = async (req, res) => {
-    try {
-        const { id } = req.params;
-        // const blog = await blogService.deleteBlog(id);
-        const blog = await Blog.findOneAndDelete({_id: id})
-        if (!blog) {
-        return res.status(404).json({ message: "Blog not found" });
-        }
-        res.status(200).json({ message: "Blog deleted successfully" });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+  try {
+    const { id } = req.params;
+    // const blog = await blogService.deleteBlog(id);
+    const blog = await Blog.findOneAndDelete({ _id: id });
+    if (!blog) {
+      return res.status(404).json({ message: "Blog not found" });
     }
+    res.status(200).json({ message: "Blog deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 module.exports = {
-    getAllBlogs,
-    getBlogById,
-    createBlog,
-    updateBlog,
-    deleteBlog,
-    createComment,
- };
+  getAllBlogs,
+  getBlogById,
+  createBlog,
+  updateBlog,
+  deleteBlog,
+  createComment,
+};
--- src/api/v1/models/blogModel.js
+++ src/api/v1/models/blogModel.js
@@ -1,42 +1,49 @@
 const mongoose = require("mongoose");
-const schema = mongoose.Schema
+const schema = mongoose.Schema;
 
-const commentSchema = new mongoose.Schema({
-        text: {
-            type: String,
-            required: true,
-        },
-        refId: {
-            type: String,
-            required: true,
-        },
-}, { timestamps: true });
+const commentSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    refId: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-const blogSchema = new mongoose.Schema({
-
+const blogSchema = new mongoose.Schema(
+  {
     heading: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     images: {
-        type: [String], // Array of image URLs
-        default: []
+      type: [String], // Array of image URLs
+      default: [],
     },
     comments: {
-        type: [commentSchema],
-        default: []
+      type: [commentSchema],
+      default: [],
     },
     favouriteCount: {
-        type: [String],
-        default: []
+      type: [String],
+      default: [],
     },
     creatorId: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     // refId: {
     //     type: String,
@@ -46,8 +53,9 @@ const blogSchema = new mongoose.Schema({
     //     type: [],
     //     required: true,
     // },
-    
-}, { timestamps: true });
+  },
+  { timestamps: true }
+);
 
 function arrayLimit(val) {
   return val.length <= 10; // Limit the number of images to 10
