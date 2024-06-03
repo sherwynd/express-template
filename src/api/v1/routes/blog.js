ff826db3e3b21378c349bc47c8f061db3420b974
Sherwynd Liew <sherwynd123@gmail.com>
2024-06-04 02:14:29 +0800

    clean

--- src/api/v1/routes/blog.js
+++ src/api/v1/routes/blog.js
@@ -1,20 +1,20 @@
 const express = require("express");
-const upload = require('../uploadConfig/index');
+const upload = require("../uploadConfig/index");
 const router = express.Router();
 
 const {
-    getAllBlogs,
-    createBlog,
-    getBlogById,
-    updateBlog,
-    deleteBlog,
-    createComment,
-    //hideBlog,
+  getAllBlogs,
+  createBlog,
+  getBlogById,
+  updateBlog,
+  deleteBlog,
+  createComment,
+  //hideBlog,
 } = require("../controllers/blogController");
 
 router.get("/blogs", getAllBlogs);
 router.get("/blogs/:id", getBlogById);
-router.post("/blogs", upload.array('images', 5), createBlog);
+router.post("/blogs", upload.array("images", 5), createBlog);
 router.patch("/blogs/:refId/:blogId", updateBlog);
 router.delete("/blogs/:id", deleteBlog);
 router.post("/comments/:id", createComment);
